9dfabbd77a9d0bee05218aa386ff7e3af82cf1fb
Daniel A. Namur <d.namur.n@gmail.com>
2023-09-27 16:37:54 -0300

    Creando primer componente y configuracion de renderizado

--- 02-COUNTER-APP-VITE/src/main.jsx
+++ 02-COUNTER-APP-VITE/src/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+/* Componente que vamos a renderizar en nuestra aplicación */
+const App = () => {
+    return (<h1>Hola Mundo !!!</h1>)
+}
+
+/** 
+ * Configuración y renderizado de nuestra aplicación.
+ * Usamos React Strict Mode para detectar posibles problemas y advertencias.
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />

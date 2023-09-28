5611e2edac0a1d1887dcaad7c07c84fb2aa7c789
Daniel A. Namur <d.namur.n@gmail.com>
2023-09-28 10:31:47 -0300

    Tarea: crear componente con proptypes de forma independiente

--- 02-COUNTER-APP-VITE/src/main.jsx
+++ 02-COUNTER-APP-VITE/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { App } from './App'
-import { FirstApp } from './FirstApp'
+import { CounterApp } from './CounterApp';
+//import { FirstApp } from './FirstApp'
 
 import './styles.css';
 
@@ -12,7 +12,6 @@ import './styles.css';
  */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <FirstApp/>
+    <CounterApp value = { 0 }/>
   </React.StrictMode>
 )

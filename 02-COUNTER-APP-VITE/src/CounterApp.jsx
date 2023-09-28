908567fc80efd0dd8b97115c65f96d8d3d2dad77
Daniel A. Namur <d.namur.n@gmail.com>
2023-09-28 11:18:11 -0300

    Integrando al componente accion de reset con valor inicial

--- 02-COUNTER-APP-VITE/src/CounterApp.jsx
+++ 02-COUNTER-APP-VITE/src/CounterApp.jsx
@@ -14,8 +14,12 @@ export const CounterApp = ({ value }) => {
 
   const handleSubtract = () => {
     if (counter > 0) {
-      setCounter (counter - 1);
+      setCounter(counter - 1);
     }
+  };
+
+  const handleReset = () => {
+    setCounter(value);
   };
 
   return (
@@ -24,8 +28,9 @@ export const CounterApp = ({ value }) => {
         <Col>
           <h1 className="text-center">CounterApp</h1>
           <h2 className="text-center">{counter}</h2>
-          <div className="d-flex justify-content-center">
+          <div className="d-flex justify-content-center mt-4">
             <Button variant='danger' onClick={handleSubtract} className="mx-2">Decrementar</Button>
+            <Button variant='warning' onClick={handleReset} className="mx-2">Reset</Button>
             <Button variant='primary' onClick={handleAdd} className="mx-2">Incrementar</Button>
           </div>
         </Col>  

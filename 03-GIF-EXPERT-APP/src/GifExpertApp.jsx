042f797a53030df1bbf67a43f330f4cb10749511
Daniel A. Namur <d.namur.n@gmail.com>
2023-10-01 19:16:01 -0300

    Siguiendo la recomendación del curso, hemos eliminado el uso de la clave (key) para validar la unicidad de las categorías. En su lugar, hemos mejorado nuestra función "onAddCategory" para incorporar validaciones adicionales que evitan la duplicación de categorías.

--- 03-GIF-EXPERT-APP/src/GifExpertApp.jsx
+++ 03-GIF-EXPERT-APP/src/GifExpertApp.jsx
@@ -8,7 +8,16 @@ export const GifExpertApp = () => {
 
     // Función para agregar una nueva categoría
     const onAddCategory = (newCategory) => {
-        setCategories([newCategory, ...categories]);
+        // Convierte la primera letra en mayúscula y el resto en minúscula
+        const formattedCategory = newCategory.charAt(0).toUpperCase() + newCategory.slice(1).toLowerCase();
+
+        // Convierte todo a minúsculas antes de hacer la comparación
+        const lowercaseCategories = categories.map((category) => category.toLowerCase());
+        const lowercaseNewCategory = formattedCategory.toLowerCase();
+
+        if (lowercaseCategories.includes(lowercaseNewCategory)) return;
+
+        setCategories([formattedCategory, ...categories]);
     }
 
     return (
@@ -23,8 +32,8 @@ export const GifExpertApp = () => {
                     {/* Listado de Gif */}
                     <ol>
                         { 
-                            categories.map((category, index) => {
-                                return <li key={index + category} className="text-light">{category}</li>;
+                            categories.map((category) => {
+                                return <li key={category} className="text-light">{category}</li>;
                             })
                         }
                     </ol>

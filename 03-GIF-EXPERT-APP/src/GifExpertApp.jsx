2c831d93231a34f69a98acf962d1fdebe6be2026
Daniel A. Namur <d.namur.n@gmail.com>
2023-10-01 18:21:18 -0300

    Tarea, crear un boton para agregar nueva categoria. Agrege funcionalidad para tomar valor de un input utilizando hook para controlar su estado y asi agregar una categoria a gusto

--- 03-GIF-EXPERT-APP/src/GifExpertApp.jsx
+++ 03-GIF-EXPERT-APP/src/GifExpertApp.jsx
@@ -1,6 +1,18 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'Dwight' ]);
+    const [inputValue, setInputValue] = useState('');
+
+    const onAddCategory = () => {
+        if (inputValue.trim() !== '') {
+            setCategories([inputValue, ...categories]);
+            setInputValue('');
+        }
+    }
+
     return (
         <Container>
             <Row>
@@ -8,8 +20,19 @@ export const GifExpertApp = () => {
                     <h1 className="text-center text-light">GifExpertApp</h1>
                     
                     {/* Input */}
+                    <div className="d-flex">
+                        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Nueva Categoria" className="form-control"/>
+                        <Button className="btn-success" onClick={onAddCategory}>Agregar categoria</Button>
+                    </div>
                     
                     {/* Listado de Gif */}
+                    <ol>
+                        { 
+                            categories.map((category, index) => {
+                                return <li key={index + category} className="text-light">{category}</li>;
+                            })
+                        }
+                    </ol>
                 </Col>
             </Row>
         </Container>

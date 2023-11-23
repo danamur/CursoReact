53d950b2e140f3ea1b2b1bff3c8e262155ff77c0
Danamur <d.namur.n@gmail.com>
2023-11-23 11:18:03 -0300

    Modificacion estructura + animaciones
    Se modifica la estructura del la pagina del heroe de una lista a parrafos; Se integran animacionacion con animate.css

--- 05-HEROS-SPA/src/heroes/components/Pagination.jsx
+++ 05-HEROS-SPA/src/heroes/components/Pagination.jsx
@@ -1,6 +1,6 @@
 export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     return (
-        <div className="pagination my-5 justify-content-center">
+        <div className="pagination py-5 justify-content-center">
             <button
                 className="btn btn-outline-secondary"
                 onClick={() => onPageChange(currentPage - 1)}
--- 05-HEROS-SPA/src/heroes/pages/HeroPage.jsx
+++ 05-HEROS-SPA/src/heroes/pages/HeroPage.jsx
@@ -1,46 +1,63 @@
-import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroeById } from "../helpers";
 import { useNavbarHeight } from "../hooks";
+import { useMemo } from "react";
 
 export const HeroPage = () => {
-
   const navbarHeight = useNavbarHeight();
   const navigate = useNavigate();
 
   const { name } = useParams();
 
-  const heroe = getHeroeById(name);
+  const heroe = useMemo( () => getHeroeById(name), [ name ]);
 
   const onNavigateBack = () => {
     navigate(-1);
-  }
+  };
 
   if (!heroe) {
-    return <Navigate to={'/marvel'} />;
+    return <Navigate to={"/marvel"} />;
   }
 
   return (
     <>
       <div className="container" style={{ marginTop: `${navbarHeight}px` }}>
         <div className="row">
-          <div className="col-md-12 text-center mb-5">
-            <h1>{ heroe.name }</h1>
+          <div className="col-md-12 text-center">
+            <h1>{heroe.name}</h1>
           </div>
-          <div className="col-md-4">
+          <div className="col-md-4 animate__animated animate__fadeInLeftBig animate__slow">
             <img src={heroe.images.lg} alt="" className="img-thumbnail" />
           </div>
-          <div className="col-md-8">
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item list-group-item-action list-group-item-dark"><b>Fullname:</b> { heroe.biography.fullName } </li>
-              <li className="list-group-item list-group-item-action list-group-item-dark"><b>Alte ego:</b> { heroe.biography.alterEgos } </li>
-              <li className="list-group-item list-group-item-action list-group-item-dark"><b>Publisher:</b> { heroe.biography.publisher } </li>
-              <li className="list-group-item list-group-item-action list-group-item-dark"><b>First Appearance:</b> { heroe.biography.firstAppearance } </li>
-              <li className="list-group-item list-group-item-action list-group-item-dark"><b>Work:</b> { heroe.work.occupation   } </li>
-              <li className="list-group-item list-group-item-action list-group-item-dark"><b>Height:</b> { heroe.appearance.height[1] } </li>
-              <li className="list-group-item list-group-item-action list-group-item-dark"><b>Weight:</b> { heroe.appearance.weight[1] } </li>
-            </ul>
+          <div className="col-md-8 animate__animated animate__lightSpeedInLeft">
+            <p>
+              <b>Fullname:</b> {heroe.biography.fullName}
+            </p>
+            <p>
+              <b>Alte ego:</b> {heroe.biography.alterEgos}
+            </p>
+            <p>
+              <b>Publisher:</b> {heroe.biography.publisher}{" "}
+            </p>
+            <p>
+              <b>First Appearance:</b> {heroe.biography.firstAppearance}{" "}
+            </p>
+            <p>
+              <b>Work:</b> {heroe.work.occupation}{" "}
+            </p>
+            <p>
+              <b>Height:</b> {heroe.appearance.height[1]}{" "}
+            </p>
+            <p>
+              <b>Weight:</b> {heroe.appearance.weight[1]}{" "}
+            </p>
 
-            <button className="btn btn-outline-danger mt-5" onClick={ onNavigateBack }>Regresar</button>
+            <button
+              className="btn btn-outline-danger mt-5"
+              onClick={onNavigateBack}
+            >
+              Regresar
+            </button>
           </div>
         </div>
       </div>

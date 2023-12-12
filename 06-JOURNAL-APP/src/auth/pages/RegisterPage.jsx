4c5fe83322f669d4b27a7ae9a0fa8c245f44ae56
Danamur <d.namur.n@gmail.com>
2023-12-11 21:11:34 -0300

    Creacion de layout de formularios para inicio de sesion y registro

--- 06-JOURNAL-APP/src/auth/pages/RegisterPage.jsx
+++ 06-JOURNAL-APP/src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,38 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+import { Google } from '@mui/icons-material';
+
 export const RegisterPage = () => {
     return (
-        <h1>RegisterPage</h1>
+        <AuthLayout title='Registrate'>
+            <form>
+                <Grid container>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField label='Nombre Completo' type='text' placeholder='John Doe' fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField label='Correo' type='email' placeholder='correo@ejemplo.cl' fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField label='Contraseña' type='password' placeholder='Contraseña' fullWidth />
+                    </Grid>
+                    <Grid container spacing={2} sx={{ mb: 2, mt: 2 }}>
+                        <Grid item xs={12} sm={12}>
+                            <Button variant='contained' fullWidth>
+                                Registrate
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container direction='row' justifyContent='end'>
+                        <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+                        <Link component={RouterLink} color='inherit' to='/auth/login'>
+                            Ingresar
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
     );
 };

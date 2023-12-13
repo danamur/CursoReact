8120846e14f19260ec7b89029e96eaec618759de
Danamur <d.namur.n@gmail.com>
2023-12-13 17:27:12 -0300

    Boton flotante para agregar contenido
    Se agrego boton flotante para agregar contenido y ademas una condicionar para ocultar el sidebar cuando las pantallas son menores a sm

--- 06-JOURNAL-APP/src/journal/components/SideBar.jsx
+++ 06-JOURNAL-APP/src/journal/components/SideBar.jsx
@@ -1,36 +1,38 @@
 import { TurnedInNot } from '@mui/icons-material';
-import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { Box, Divider, Drawer, Grid, Hidden, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 
 export const SideBar = ({ drawerWidth = 240 }) => {
     return (
         <Box component='nav' sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
-            <Drawer variant='permanent' open={true} sx={{ display: { xs: 'block' }, '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth } }}>
-                <Toolbar>
-                    <Typography variant='h6' noWrap component='div'>
-                        Daniel Namur
-                    </Typography>
-                </Toolbar>
-                <Divider />
+            <Hidden smDown>
+                <Drawer variant='permanent' open={true} sx={{ display: { xs: 'block' }, '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth } }}>
+                    <Toolbar>
+                        <Typography variant='h6' noWrap component='div'>
+                            Daniel Namur
+                        </Typography>
+                    </Toolbar>
+                    <Divider />
 
-                <List>
-                    {
-                        ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
-                            <ListItem key={text}>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <TurnedInNot />
-                                    </ListItemIcon>
-                                    <Grid>
-                                        <ListItemText primary={text} />
-                                        <ListItemText secondary={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
-                                    </Grid>
-                                </ListItemButton>
-                            </ListItem>
-                        ))
-                    }
-                </List>
+                    <List>
+                        {
+                            ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
+                                <ListItem key={text}>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            <TurnedInNot />
+                                        </ListItemIcon>
+                                        <Grid>
+                                            <ListItemText primary={text} />
+                                            <ListItemText secondary={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
+                                        </Grid>
+                                    </ListItemButton>
+                                </ListItem>
+                            ))
+                        }
+                    </List>
 
-            </Drawer>
+                </Drawer>
+            </Hidden>
         </Box>
     );
 };

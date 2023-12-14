0adf5642e134432385d5ce74d4ffb8936d9fdafb
Danamur <d.namur.n@gmail.com>
2023-12-13 21:51:28 -0300

    Inicianilizando funcion increment en la app

--- 07-TOOLKIT-REDUX/src/store/slices/counter/counterSlice.js
+++ 07-TOOLKIT-REDUX/src/store/slices/counter/counterSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
-    value: 10,
+    counter: 10,
   },
   reducers: {
     increment: (state) => {
@@ -11,13 +11,13 @@ export const counterSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1
+      state.counter += 1
     },
     decrement: (state) => {
-      state.value -= 1
+      state.counter -= 1
     },
     incrementByAmount: (state, action) => {
-      state.value += action.payload
+      state.counter += action.payload
     },
   },
 })

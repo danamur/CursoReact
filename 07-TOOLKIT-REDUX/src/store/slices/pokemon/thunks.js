3624808f508562f83c51b8ca9cbf30dbf2b4ef2a
Danamur <d.namur.n@gmail.com>
2023-12-14 17:34:20 -0300

    Integrando axios
    Utilizamos axios para realizar la peticion a la api de pokemon y obtener los primeros 10 pokemons y enviarlo al slices

--- 07-TOOLKIT-REDUX/src/store/slices/pokemon/thunks.js
+++ 07-TOOLKIT-REDUX/src/store/slices/pokemon/thunks.js
@@ -1,7 +1,16 @@
-import { startLoadingPokemons } from './pokemonSlice';
+import { pokemonApi } from '../../../api/pokemonApi';
+import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 export const getPokemons = (page = 0) => {
-    return (dispatch, getState) => {
+    return async (dispatch, getState) => {
         dispatch(startLoadingPokemons());
+
+        // Ejmplo de peticion con fetch
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+        // const data = await resp.json();
+
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
+
+        dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
     };
 };

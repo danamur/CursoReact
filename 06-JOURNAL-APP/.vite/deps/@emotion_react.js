38537a82d9025519899f76407f1695bdffc092e9
Danamur <d.namur.n@gmail.com>
2023-12-13 21:13:52 -0300

    Inicializando proyecto Toolkit-redux
    Se inializa proyecto para conecer y practicar el funcionamiento de redux. Se levanta el proyecto y se realiza instalacion de redux

---
+++ 06-JOURNAL-APP/.vite/deps/@emotion_react.js
@@ -0,0 +1,32 @@
+import {
+  CacheProvider,
+  ClassNames,
+  Global,
+  ThemeContext,
+  ThemeProvider,
+  __unsafe_useEmotionCache,
+  css,
+  jsx,
+  keyframes,
+  useTheme,
+  withEmotionCache,
+  withTheme
+} from "./chunk-AGYXYGLO.js";
+import "./chunk-67XTWVEJ.js";
+import "./chunk-5WWUZCGV.js";
+export {
+  CacheProvider,
+  ClassNames,
+  Global,
+  ThemeContext,
+  ThemeProvider,
+  __unsafe_useEmotionCache,
+  jsx as createElement,
+  css,
+  jsx,
+  keyframes,
+  useTheme,
+  withEmotionCache,
+  withTheme
+};
+//# sourceMappingURL=@emotion_react.js.map

2bdd8330a692a9ca0dc905d4158f0698b674963b
Danamur <d.namur.n@gmail.com>
2023-12-31 13:18:22 -0300

    Validando desde nuestro custom hook de forma dinamica los input

--- 06-JOURNAL-APP/src/auth/pages/LoginPage.jsx
+++ 06-JOURNAL-APP/src/auth/pages/LoginPage.jsx
@@ -57,7 +57,7 @@ export const LoginPage = () => {
                     </Grid>
 
                     <Grid container direction='row' justifyContent='end'>
-                        <Link component={RouterLink} color='inherit' to='/auth/register'>
+                        <Link component={RouterLink} to='/auth/register'>
                             Crear una cuenta
                         </Link>
                     </Grid>
--- 06-JOURNAL-APP/src/auth/pages/RegisterPage.jsx
+++ 06-JOURNAL-APP/src/auth/pages/RegisterPage.jsx
@@ -1,25 +1,47 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
-import { Google } from '@mui/icons-material';
+import { useForm } from '../../hooks';
+
+const formData = {
+    email: 'daniel.namur.dev@gmail.com',
+    password: '123456',
+    name: 'Daniel Namur'
+};
+
+const formValidations = {
+    email: [(value) => value.includes('@'), 'El correo debe tener un @'],
+    password: [(value) => value.length >= 6, 'La contraseña debe tener al mas de 6 caracteres'],
+    name: [(value) => value.length >= 1, 'El nombre es obligatorio']
+}
 
 export const RegisterPage = () => {
+
+    const { name, email, password, onInputChange, formState, isFormValid, nameValid, emailValid, passwordValid } = useForm(formData, formValidations);
+
+    console.log(nameValid);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <AuthLayout title='Registrate'>
-            <form>
+            <form onSubmit={onSubmit}>
                 <Grid container>
                     <Grid item xs={12} sx={{ mt: 2 }}>
-                        <TextField label='Nombre Completo' type='text' placeholder='John Doe' fullWidth />
+                        <TextField label='Nombre Completo' type='text' placeholder='John Doe' fullWidth name='name' value={name} onChange={onInputChange} />
                     </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
-                        <TextField label='Correo' type='email' placeholder='correo@ejemplo.cl' fullWidth />
+                        <TextField label='Correo' type='email' placeholder='correo@ejemplo.cl' fullWidth name='email' value={email} onChange={onInputChange} />
                     </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
-                        <TextField label='Contraseña' type='password' placeholder='Contraseña' fullWidth />
+                        <TextField label='Contraseña' type='password' placeholder='Contraseña' fullWidth name='password' value={password} onChange={onInputChange} />
                     </Grid>
                     <Grid container spacing={2} sx={{ mb: 2, mt: 2 }}>
                         <Grid item xs={12} sm={12}>
-                            <Button variant='contained' fullWidth>
+                            <Button type='submit' variant='contained' fullWidth>
                                 Registrate
                             </Button>
                         </Grid>
@@ -27,7 +49,7 @@ export const RegisterPage = () => {
 
                     <Grid container direction='row' justifyContent='end'>
                         <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
-                        <Link component={RouterLink} color='inherit' to='/auth/login'>
+                        <Link component={RouterLink} to='/auth/login'>
                             Ingresar
                         </Link>
                     </Grid>

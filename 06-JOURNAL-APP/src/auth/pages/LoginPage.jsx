5e8a69e8779a03167fbdb4029bcee7039c6b454b
Danamur <d.namur.n@gmail.com>
2024-01-08 22:49:34 -0300

    Mostrando mensajes de error en login con credenciales incorrectas

--- 06-JOURNAL-APP/src/auth/pages/LoginPage.jsx
+++ 06-JOURNAL-APP/src/auth/pages/LoginPage.jsx
@@ -1,17 +1,17 @@
 import { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
-import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Alert, Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
 import { AuthLayout } from '../layout/AuthLayout';
 import { useForm } from '../../hooks';
-import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
+import { checkingAuthentication, startGoogleSignIn, startLoginWithEmailPassword } from '../../store/auth';
 
 export const LoginPage = () => {
 
     const dispatch = useDispatch();
 
-    const { status } = useSelector(state => state.auth);
+    const { status, errorMessage } = useSelector(state => state.auth);
 
     const { email, password, onInputChange } = useForm({
         email: 'daniel.namur.dev@gmail.com',
@@ -23,9 +23,7 @@ export const LoginPage = () => {
     const onSubmit = (event) => {
         event.preventDefault();
 
-        console.log({ email, password });
-
-        dispatch(checkingAuthentication({ email, password }));
+        dispatch(startLoginWithEmailPassword({ email, password }));
     };
 
     const onGoogleSingIn = () => {
@@ -42,6 +40,13 @@ export const LoginPage = () => {
                     <Grid item xs={12} sx={{ mt: 2 }}>
                         <TextField label='Contraseña' type='password' placeholder='Contraseña' fullWidth name='password' value={password} onChange={onInputChange} />
                     </Grid>
+
+                    <Grid container display={ !!errorMessage ? '' : 'none' } sx={{ mt: 2 }}>
+                        <Grid item xs={12} >
+                            <Alert severity='error'>{ errorMessage }</Alert>
+                        </Grid>
+                    </Grid>
+
                     <Grid container spacing={2} sx={{ mb: 2, mt: 2 }}>
                         <Grid item xs={12} sm={6}>
                             <Button disabled={isAuthenticating} type='submit' variant='contained' fullWidth>
--- 06-JOURNAL-APP/src/firebase/providers.js
+++ 06-JOURNAL-APP/src/firebase/providers.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth';
 import { FirebaseAuth } from './config';
 
 const googleProvider = new GoogleAuthProvider();
@@ -44,5 +44,23 @@ export const registerUserWithEmailPassword = async ({ email, password, displayNa
             ok: false,
             errorMessage: error.message
         }
+    }
+}
+
+export const loginWithEmailPassword = async ({ email, password }) => {
+    try {
+        const resp = await signInWithEmailAndPassword(FirebaseAuth, email, password);
+        const { uid, photoURL, displayName } = resp.user;
+
+        return {
+            ok: true,
+            uid, photoURL, email, displayName
+        }
+    } catch (error) {
+        console.log(error);
+        return {
+            ok: false,
+            errorMessage: error.message
+        };
     }
 }

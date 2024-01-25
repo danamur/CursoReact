c7192a3fb3f59f75e1ad8dcfcbcf3dbefe39cd5b
Danamur <d.namur.n@gmail.com>
2024-01-25 13:50:19 -0300

    Integrando logout funcional en la aplicacion

--- 06-JOURNAL-APP/src/journal/components/SideBar.jsx
+++ 06-JOURNAL-APP/src/journal/components/SideBar.jsx
@@ -1,14 +1,18 @@
+import { useSelector } from 'react-redux';
 import { TurnedInNot } from '@mui/icons-material';
 import { Box, Divider, Drawer, Grid, Hidden, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 
 export const SideBar = ({ drawerWidth = 240 }) => {
+
+    const { displayName } = useSelector(state => state.auth);
+
     return (
         <Box component='nav' sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
             <Hidden smDown>
                 <Drawer variant='permanent' open={true} sx={{ display: { xs: 'block' }, '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth } }}>
                     <Toolbar>
                         <Typography variant='h6' noWrap component='div'>
-                            Daniel Namur
+                            {displayName}
                         </Typography>
                     </Toolbar>
                     <Divider />

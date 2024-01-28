a70ccdb74d042f4a5fdd20d406e3dbe7e9e92c80
Danamur <d.namur.n@gmail.com>
2024-01-27 23:04:00 -0300

    Creando JournalSlice + thunks de store journal

---
+++ 06-JOURNAL-APP/src/store/journal/journalSlice.js
@@ -0,0 +1,40 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+        isSaving: true,
+        messageSaved: '',
+        notes: [],
+        active: null
+    },
+    reducers: {
+        addNewEmptyNote: (state, action) => {
+
+        },
+        setActiveNote: (state,action) => {
+
+        },
+        setNote: (state,action) => {
+
+        },
+        setSaving: (state,action) => {
+
+        },
+        updateNote: (state,action) => {
+
+        },
+        deleteNoteById: (state,action) => {
+
+        },
+    }
+});
+
+export const { 
+    addNewEmptyNote,
+    setActiveNote,
+    setNote,
+    setSaving,
+    updateNote,
+    deleteNoteById
+} = journalSlice.actions;

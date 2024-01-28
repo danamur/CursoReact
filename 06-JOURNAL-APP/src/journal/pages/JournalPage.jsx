321ad8f54f4656337db175bbe8691454a38e281b
Danamur <d.namur.n@gmail.com>
2024-01-27 23:36:57 -0300

    Creando una nueva nota por thunks

--- 06-JOURNAL-APP/src/journal/pages/JournalPage.jsx
+++ 06-JOURNAL-APP/src/journal/pages/JournalPage.jsx
@@ -1,16 +1,25 @@
-import { IconButton } from '@mui/material';
+import { useDispatch } from 'react-redux';
 import { AddOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 
+import { startNewNote } from '../../store/journal';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
+
+    const dispatch = useDispatch();
+
+    const onClickNewNote = () => {
+        dispatch(startNewNote());
+    };
+
     return (
         <JournalLayout>
             <NothingSelectedView />
             {/* <NoteView /> */}
 
-            <IconButton sx={{ color: 'white', backgroundColor: 'error.main', ':hover': {backgroundColor: 'error.main', opacity: 0.9 }, position: 'fixed', right: 50, bottom: 50 }}>
+            <IconButton onClick={onClickNewNote} sx={{ color: 'white', backgroundColor: 'error.main', ':hover': { backgroundColor: 'error.main', opacity: 0.9 }, position: 'fixed', right: 50, bottom: 50 }}>
                 <AddOutlined sx={{ fontSize: 30 }} />
             </IconButton>
         </JournalLayout>

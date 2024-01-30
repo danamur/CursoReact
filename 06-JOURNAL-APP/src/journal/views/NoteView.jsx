7730d201ccf40b73daf6b6b36c9fa81bfd31803d
Danamur <d.namur.n@gmail.com>
2024-01-29 22:14:57 -0300

    Integrando SweetAlert 2 para mostrar mensaje de actualizacion.

--- 06-JOURNAL-APP/src/journal/views/NoteView.jsx
+++ 06-JOURNAL-APP/src/journal/views/NoteView.jsx
@@ -7,10 +7,13 @@ import { ImageGallery } from '../components';
 import { useForm } from '../../hooks/useForm';
 import { setActiveNote, startSaveNote } from '../../store/journal';
 
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
+
 export const NoteView = () => {
 
     const dispatch = useDispatch();
-    const { active: note } = useSelector(state => state.journal);
+    const { active: note, messageSaved, isSaving } = useSelector(state => state.journal);
 
     const { body, title, date, onInputChange, formState } = useForm(note);
 
@@ -23,6 +26,13 @@ export const NoteView = () => {
         dispatch(setActiveNote(formState));
     }, [formState]);
 
+    useEffect(() => {
+      if (messageSaved.length > 0) {
+        Swal.fire('Nota actualizada', messageSaved, 'success');
+      }
+    }, [messageSaved])
+    
+
     const onSaveNote = () => {
         dispatch(startSaveNote());
     }
@@ -33,7 +43,7 @@ export const NoteView = () => {
                 <Typography fontSize={39} fontWeight='light'>{dateString}</Typography>
             </Grid>
             <Grid item>
-                <Button onClick={onSaveNote} variant='contained' color='primary'>
+                <Button disabled={ isSaving } onClick={onSaveNote} variant='contained' color='primary'>
                     <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
                     Guardar
                 </Button>
